5afaedc28cb57881f501c14c2fd428073fad45ba
okediarthur <okediarthur@gmail.com>
2024-04-19 18:01:54 +0300

    Stat Board Added

--- app/ui/dashboard/chart/chart.jsx
+++ app/ui/dashboard/chart/chart.jsx
@@ -1,8 +1,71 @@
+'use client'
+// import { LineChart, ResponsiveContainer } from 'recharts';
 import styles from './chart.module.css';
+import { LineChart, Line, XAxis, YAxis, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+const data = [
+    {
+        name: "Sun",
+        answered: 4000,
+        rejected: 2400,
+    },
+    {
+        name: "Mon",
+        answered: 3000,
+        rejected: 1398,
+    },
+    {
+        name: "Tue",
+        answered: 2000,
+        rejected: 3800,
+    },
+    {
+        name: "Wed",
+        answered: 2780,
+        rejected: 3800,
+    },
+    {
+        name: "Thu",
+        answered: 1890,
+        rejected: 4800,
+    },
+    {
+        name: "Fri",
+        answered: 2390,
+        rejected: 3800,
+    },
+    {
+        name: "Sat",
+        answered: 3490,
+        rejected: 4300,
+    },
+]
 
 const Chart = () => {
     return (
-        <div className={styles.container}>Chart</div>
+        <div className={styles.container}>
+            <h2 className='styles.title'>Performance</h2>
+           <ResponsiveContainer width="90%" height="90%">
+            <LineChart
+                width={500}
+                height={300}
+                data={data}
+                margin={{
+                    top: 5,
+                    right: 30,
+                    left: 20,
+                    bottom: 5,
+            }}
+        >
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip contentStyle={{background:"#151c2c", border:"none"}} />
+                <Legend />
+                <Line type="monotone" dataKey="rejected" stroke="#8884d8" strokeDasharray="5 5" />
+                <Line type="monotone" dataKey="answered" stroke="#82ca9d" strokeDasharray="3 4 5 2" />
+            </LineChart>
+        </ResponsiveContainer>
+    </div>
     )
 }
 
--- app/ui/dashboard/stats/stats.jsx
+++ app/ui/dashboard/stats/stats.jsx
@@ -1,8 +1,95 @@
 import styles from './stats.module.css';
+import Image from 'next/image';
 
 const Stats = () => {
     return (
-        <div className={styles.container}>Stat</div>
+        <div className={styles.container}>
+            <h2 className={styles.title}>Call Statistics</h2>
+            <table className={styles.table}>
+                <thead>
+                    <tr>
+                        <td>Contact</td>
+                        <td>Campaign</td>
+                        <td>Status</td>
+                        <td>Date</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <div className={styles.user}>
+                                <Image 
+                                src="/noavatar.png" 
+                                alt="User Image" 
+                                width={40} 
+                                height={40} 
+                                className={styles.userImage}
+                            />
+                            +256784922032
+                            </div>
+                        <td>Polio Vaccination</td>
+                        <td>
+                            <span className={`${styles.status} ${styles.done}`}>done
+                            </span>
+                        </td>
+                        <td>19.04.2024</td>
+                    </tr>
+                    <tr>
+                        <div className={styles.user}>
+                                <Image 
+                                src="/noavatar.png" 
+                                alt="User Image" 
+                                width={40} 
+                                height={40} 
+                                className={styles.userImage}
+                            />
+                            +256784922032
+                            </div>
+                        <td>Malaria Sensitisation</td>
+                        <td>
+                            <span className={`${styles.status} ${styles.cancelled}`}>cancelled
+                            </span>
+                        </td>
+                        <td>1.05.2024</td>
+                    </tr>
+                    <tr>
+                        <div className={styles.user}>
+                                <Image 
+                                src="/noavatar.png" 
+                                alt="User Image" 
+                                width={40} 
+                                height={40} 
+                                className={styles.userImage}
+                            />
+                            +256784922032
+                            </div>
+                        <td>Polio Vaccination</td>
+                        <td>
+                            <span className={`${styles.status} ${styles.pending}`}>pending
+                            </span>
+                        </td>
+                        <td>14.02.2024</td>
+                    </tr>
+                    <tr>
+                        <div className={styles.user}>
+                                <Image 
+                                src="/noavatar.png" 
+                                alt="User Image" 
+                                width={40} 
+                                height={40} 
+                                className={styles.userImage}
+                            />
+                            +256784922032
+                            </div>
+                        <td>USE Resumption</td>
+                        <td>
+                            <span className={`${styles.status} ${styles.done}`}>done
+                            </span>
+                        </td>
+                        <td>18.03.2024</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     )
 }
 
