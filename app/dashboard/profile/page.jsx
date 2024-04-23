c34b1d40f3934a8b823a9e42688aec3bc3cff698
okediarthur <okediarthur@gmail.com>
2024-04-23 09:56:45 +0300

    Adjustments made

--- app/dashboard/profile/page.jsx
+++ app/dashboard/profile/page.jsx
@@ -9,7 +9,7 @@ const  Profile = () => {
         <div className={styles.container}>
             <div className={styles.top}>
                 <Search placeholder="Search for a user..." />
-                <Link href="/dashboard/users/add">
+                <Link href="/dashboard/profile/add">
                     <button className={styles.addButton}>Add New</button>
                 </Link>
             </div>
